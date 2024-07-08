16fecfff703d0fe8c1f3bbc99031a8989f290712
mirna <mirna@DESKTOP-NVAPE0C>
2024-07-08 23:22:39 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        #region q1
+     /*   public enum days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }*/
+        #endregion
+
+        #region q2:
+
+        #endregion
+        static void Main()
+        {
+            #region q1:
+         /*   foreach (days day in Enum.GetValues(typeof(days)))
+            {
+                Console.WriteLine(day);
+            } */
+            #endregion
+
+        }
+    }
+}
